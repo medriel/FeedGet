5d82ae47f99fe0eecef63b2ab330134b7ddd5e71
medriel <adriel.souza397@gmail.com>
2022-05-02 11:56:34 -0300

    Adicionando acessibilidade no button Widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,19 +1,14 @@
-import React, { useState } from "react"
 import { ChatTeardropDots } from "phosphor-react"
+import { Popover } from '@headlessui/react'
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
 
   return (
-    <div className="absolute bottom-5 right-5">
+    <Popover className="absolute bottom-5 right-5">
 
-      {isWidgetOpen && <p>Modal</p>}
+      <Popover.Panel>Modal</Popover.Panel>
 
-      <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 
+      <Popover.Button className="bg-brand-500 rounded-full px-3 
       h-12 text-white flex items-center group">
         <ChatTeardropDots className="w-6 h-6" />
 
@@ -22,7 +17,7 @@ export function Widget() {
           <span className="pl-2"></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover >
   )
 }
