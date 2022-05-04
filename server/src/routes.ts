fd3181bab062406fa1ea6dcfc60fec9654a1777b
medriel <adriel.souza397@gmail.com>
2022-05-04 11:22:04 -0300

    Configurando o jest

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import { NodemailerMailAdapter } from "./adapters/nodemailer/nodemailer-mail-adapter";
 
-import { prisma } from "./prisma";
 import { PrismaFeedbacksRepository } from "./repositories/prisma/prisma-feedbacks-repository";
 import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 
