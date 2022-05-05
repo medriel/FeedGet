b1afd98c79a0f2c79080daf3c3d6242ebcbb08cb
medriel <adriel.souza397@gmail.com>
2022-05-05 20:11:49 -0300

    Desenvolvendo a navegação entre componentes

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -3,11 +3,15 @@ import { View, Text } from "react-native";
 import { feedbackTypes } from "../../utils/feedbackTypes";
 import { Copyright } from "../Copyright";
 import { Option } from "../Option";
+import { FeedbackType } from "../Widget";
 
 import { styles } from "./styles";
 
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
 
-export function Options() {
+export function Options({ onFeedbackTypeChanged }: Props) {
   return (
     <View style={styles.container}>
 
@@ -22,6 +26,7 @@ export function Options() {
                 key={key}
                 title={value.title}
                 image={value.image}
+                onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
               />
             ))
         }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react"; import { TouchableOpacity } from "react-native";
+import React, { useRef, useState } from "react"; import { TouchableOpacity } from "react-native";
 import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
@@ -14,11 +14,22 @@ import { feedbackTypes } from "../../utils/feedbackTypes";
 export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
   const bottomSheetRef = useRef<BottomSheet>(null);
 
   function handleOpen() {
     bottomSheetRef.current?.expand();
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true);
   }
 
   return (
@@ -39,9 +50,25 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Form
-          feedbackType="BUG"
-        />
+        {
+          feedbackSent ?
+            <Success />
+            :
+            <>
+              {
+                feedbackType ?
+                  <Form
+                    feedbackType={feedbackType}
+                    onFeedbackCanceled={handleRestartFeedback}
+                    onFeedbackSent={handleFeedbackSent}
+                  />
+                  :
+                  <Options
+                    onFeedbackTypeChanged={setFeedbackType}
+                  />
+              }
+            </>
+        }
       </BottomSheet>
     </>
   )
