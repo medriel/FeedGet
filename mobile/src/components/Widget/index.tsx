967b20ba22e061d78cc591899bd7711e7f08478b
medriel <adriel.souza397@gmail.com>
2022-05-05 09:34:10 -0300

    COnfigurando fonts personalizadas

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
+
+export function Widget() {
+  return (
+    <View style={styles.container}></View>
+  )
+}
