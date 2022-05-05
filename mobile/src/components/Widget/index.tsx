ee5a108dcac5760e5a078e7e76187b674ae60cc0
medriel <adriel.souza397@gmail.com>
2022-05-05 10:39:58 -0300

    Criando o footer do modal

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -3,6 +3,8 @@ import { ChatTeardropDots } from "phosphor-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
+import { Options } from "../Options";
+
 import { styles } from "./styles";
 import { theme } from "../../theme";
 
@@ -32,7 +34,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-
+        <Options />
       </BottomSheet>
     </>
   )
